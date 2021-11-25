1c1f1d2055ba776f609ba5d309bc81d1f97debd0
Alvaro Garcia <alvaro.garcia@ucdconnect.ie>
2021-11-25 21:04:19 +0100

    change webpack

--- next.config.js
+++ next.config.js
@@ -1,26 +1,4 @@
 module.exports = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-    };
-
-    // load worker files as a urls with `file-loader`
-    config.module.rules.push({
-      test: /pdf\.worker\.(min\.)?js/,
-      use: [
-        {
-          loader: "file-loader",
-          options: {
-            name: "[contenthash].[ext]",
-            publicPath: "/_next/static/worker",
-            outputPath: "static/worker"
-          }
-        }
-      ]
-    });
-
-    return config;
-  }
+  reactStrictMode: true
 };
 
