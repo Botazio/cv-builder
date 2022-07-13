0847427b81cd9810ab32a2e8b8ad6fd4cb5ac5a3
Alvaro Garcia <alvaro.garcia@ucdconnect.ie>
2022-07-13 03:08:07 +0200

    Finish the layout for the privacy & cookies policy

--- src/modules/pages/legal/privacy-cookies-policy-page.tsx
+++ src/modules/pages/legal/privacy-cookies-policy-page.tsx
@@ -1,9 +1,21 @@
-import LayoutDefault from "common/components/layouts/layout-default/layout-default";
+import SpaceDivider from "common/components/atoms/dividers/space-divider/space-divider";
+import LayoutCard from "common/components/layouts/layout-card/layout-card";
+import LayoutCardBody from "common/components/layouts/layout-card/layout-card-body/layout-card-body";
+import LayoutCardHeader from "common/components/layouts/layout-card/layout-card-header/layout-card-header";
 
 export default function PrivacyCookiesPolicyPage() {
   return (
-    <LayoutDefault>
+    <LayoutCard header={
+      <>
+        <SpaceDivider variant="large" />
+        <LayoutCardHeader title="Privacy & cookies policy" />
+      </>
+    }>
 
-    </LayoutDefault>
+      <LayoutCardBody>
+        <p>hello</p>
+      </LayoutCardBody>
+
+    </LayoutCard>
   );
 }
